bfecb42fe289696a4284622902b5bc8173a008a1
PreranaLatha <129479795+PreranaLatha@users.noreply.github.com>
2025-07-30 19:56:56 +0530

    Time: 6 ms (25.87%) | Memory: 43.1 MB (57.34%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
@@ -1,16 +1,18 @@
 public class Codec {
 
+    private Map<Integer, String> map = new HashMap<>();
+    private static final String BASE_HOST = "http://tinyurl.com/";
+    private int id = 1;
+
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-         return longUrl;
+        map.put(id, longUrl);
+        return BASE_HOST + id++;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        int key = Integer.parseInt(shortUrl.replace(BASE_HOST, ""));
+        return map.get(key);
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(url));
