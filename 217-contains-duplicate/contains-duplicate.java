d42a094a05420cefa3dee015c332c6c9c60fda11
PreranaLatha <129479795+PreranaLatha@users.noreply.github.com>
2026-06-21 13:17:38 +0530

    Time: 15 ms (62.77%) | Memory: 93.5 MB (40.91%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> h=new HashSet<>();
+        for(int i:nums){
+            if(h.contains(i))return true;
+            h.add(i);
+        }return false;
+    }
+}
